2ae08ac2d0266cb7a57ff61882c8fbabcabf89f2
Vipin <vkm.developer@gmail.com>
2020-11-13 13:16:11 +0530

    commited

--- utility.php
+++ utility.php
@@ -25,7 +25,8 @@ const IMAGE_HANDLERS = [
  * @param $targetWidth - desired output width
  * @param $targetHeight - desired output height or null
  */
-function createThumbnail($src, $dest, $targetWidth, $targetHeight = null) {
+function createThumbnail($src, $dest, $targetWidth, $targetHeight = null)
+{
 
     // 1. Load the image from the given $src
     // - see if the file actually exists
@@ -103,9 +104,14 @@ function createThumbnail($src, $dest, $targetWidth, $targetHeight = null) {
     imagecopyresampled(
         $thumbnail,
         $image,
-        0, 0, 0, 0,
-        $targetWidth, $targetHeight,
-        $width, $height
+        0,
+        0,
+        0,
+        0,
+        $targetWidth,
+        $targetHeight,
+        $width,
+        $height
     );
 
 
@@ -130,21 +136,107 @@ createThumbnail('profile.jpg', 'profile_thumb.jpg', 160);
  * clean array data before insert to db
  */
 if (!function_exists('mysql_clean_array')) {
-	function mysql_clean_array(&$data) {
-		array_walk_recursive($data, function (&$val, $key) {
-			$val = mysql_real_escape_string($val);
-		});
-	}
+    function mysql_clean_array(&$data)
+    {
+        array_walk_recursive($data, function (&$val, $key) {
+            $val = mysql_real_escape_string($val);
+        });
+    }
 }
 // clean data 
 // $request = $_REQUEST;
 // mysql_clean_array($request); // use $request instead of $_REQUEST
 
 if (!function_exists('fileLog')) {
-    function fileLog($msg, $fileName='') {
+    function fileLog($msg, $fileName = '')
+    {
         $fileName = ($fileName) ? $fileName : "log-file.txt";
         $logFile = fopen($fileName, "a+") or die("Unable to open file!");
         fwrite($logFile, $msg);
         fclose($logFile);
     }
+}
+
+function numberTowords($num)
+{
+
+    $ones = array(
+        0 => "ZERO",
+        1 => "ONE",
+        2 => "TWO",
+        3 => "THREE",
+        4 => "FOUR",
+        5 => "FIVE",
+        6 => "SIX",
+        7 => "SEVEN",
+        8 => "EIGHT",
+        9 => "NINE",
+        10 => "TEN",
+        11 => "ELEVEN",
+        12 => "TWELVE",
+        13 => "THIRTEEN",
+        14 => "FOURTEEN",
+        15 => "FIFTEEN",
+        16 => "SIXTEEN",
+        17 => "SEVENTEEN",
+        18 => "EIGHTEEN",
+        19 => "NINETEEN",
+        "014" => "FOURTEEN"
+    );
+    $tens = array(
+        0 => "ZERO",
+        1 => "TEN",
+        2 => "TWENTY",
+        3 => "THIRTY",
+        4 => "FORTY",
+        5 => "FIFTY",
+        6 => "SIXTY",
+        7 => "SEVENTY",
+        8 => "EIGHTY",
+        9 => "NINETY"
+    );
+    $hundreds = array(
+        "HUNDRED",
+        "THOUSAND",
+        "MILLION",
+        "BILLION",
+        "TRILLION",
+        "QUARDRILLION"
+    ); /*limit t quadrillion */
+    $num = number_format($num, 2, ".", ",");
+    $num_arr = explode(".", $num);
+    $wholenum = $num_arr[0];
+    $decnum = $num_arr[1];
+    $whole_arr = array_reverse(explode(",", $wholenum));
+    krsort($whole_arr, 1);
+    $rettxt = "";
+    foreach ($whole_arr as $key => $i) {
+
+        while (substr($i, 0, 1) == "0")
+            $i = substr($i, 1, 5);
+        if ($i < 20) {
+            /* echo "getting:".$i; */
+            $rettxt .= $ones[$i];
+        } elseif ($i < 100) {
+            if (substr($i, 0, 1) != "0")  $rettxt .= $tens[substr($i, 0, 1)];
+            if (substr($i, 1, 1) != "0") $rettxt .= " " . $ones[substr($i, 1, 1)];
+        } else {
+            if (substr($i, 0, 1) != "0") $rettxt .= $ones[substr($i, 0, 1)] . " " . $hundreds[0];
+            if (substr($i, 1, 1) != "0") $rettxt .= " " . $tens[substr($i, 1, 1)];
+            if (substr($i, 2, 1) != "0") $rettxt .= " " . $ones[substr($i, 2, 1)];
+        }
+        if ($key > 0) {
+            $rettxt .= " " . $hundreds[$key] . " ";
+        }
+    }
+    if ($decnum > 0) {
+        $rettxt .= " and ";
+        if ($decnum < 20) {
+            $rettxt .= $ones[$decnum];
+        } elseif ($decnum < 100) {
+            $rettxt .= $tens[substr($decnum, 0, 1)];
+            $rettxt .= " " . $ones[substr($decnum, 1, 1)];
+        }
+    }
+    return $rettxt;
 }
